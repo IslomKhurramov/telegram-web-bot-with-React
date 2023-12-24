9e3a8f77a2ae367765ba9a91888b0c9628961736
IslomKhurramov <islom.khurramov@gmail.com>
2023-12-25 07:30:31 +0900

    feat: russian menu part1

--- src/components/constants/db.js
+++ src/components/constants/db.js
@@ -1,37 +1,37 @@
 export function getData() {
   return [
     {
-      title: "Beef Shurpa",
+      title: "Beef Shurpa (Шурпа из говядины)",
       price: 9000,
       Image: "./foods/soups/beefshurpa.png",
       id: 1,
     },
     {
-      title: "Red-beef-soup",
+      title: "Red-beef-soup (Борщ)",
       price: 8000,
       Image: "./foods/soups/borsh.png",
       id: 2,
     },
     {
-      title: "Dumplings with soup",
+      title: "Dumplings with soup (Пельмени с бульоном)",
       price: 8000,
       Image: "./foods/soups/dumplingsoup.png",
       id: 3,
     },
     {
-      title: "lagmon",
+      title: "lagmon (Лагман)",
       price: 8000,
       Image: "./foods/soups/lagman.png",
       id: 4,
     },
     {
-      title: "Lamb shurpa",
+      title: "Lamb shurpa (Шурпа из баранины)",
       price: 9000,
       Image: "./foods/soups/lambshurpa.png",
       id: 5,
     },
     {
-      title: "Rice soup",
+      title: "Rice soup (Мастава)",
       price: 18000,
       Image: "./foods/soups/mastava.png",
       id: 6,
@@ -43,7 +43,7 @@ export function getData() {
       id: 7,
     },
     {
-      title: "Soup with meatballs",
+      title: "Soup with meatballs (Суп с фрикадельками)",
       price: 8000,
       Image: "./foods/soups/meatball.png",
       id: 8,
