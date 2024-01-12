a7e8e5cc96c49e9b43819f855245473060618b44
IslomKhurramov <islom.khurramov@gmail.com>
2024-01-13 03:11:39 +0900

    feat:updating12

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -81,25 +81,25 @@ const PaymentForm = (props) => {
       // Capture the pictureId from the response
       const pictureId = response.data.pictureId;
 
+      // Update userDataRef with the user information, including pictureId
+      userDataRef.current = {
+        name,
+        number,
+        deliveryOption,
+        address: deliveryOption === "delivery" ? address : "",
+        paymentOption,
+        pictureId, // Add the pictureId to the userData
+      };
+      // Update teleg.MainButton properties if needed
+      teleg.MainButton.text = "Submit";
+      teleg.MainButton.show();
+
       // Handle the response from the backend as needed
       console.log("File uploaded successfully. Picture ID:", pictureId);
     } catch (error) {
       console.error("Error during file upload:", error);
       // Handle the error, show a message to the user, etc.
     }
-
-    // Update userDataRef with the user information, including pictureId
-    userDataRef.current = {
-      name,
-      number,
-      deliveryOption,
-      address: deliveryOption === "delivery" ? address : "",
-      paymentOption,
-      pictureId, // Add the pictureId to the userData
-    };
-    // Update teleg.MainButton properties if needed
-    teleg.MainButton.text = "Submit";
-    teleg.MainButton.show();
   };
 
   useEffect(() => {
